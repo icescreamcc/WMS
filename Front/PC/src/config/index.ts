2ae7b04c9bb35c236827a3b7bf5a4e5fdc602eec
grb2015 <949409706@qq.com>
2025-09-16 14:46:42 +0800

    1.浏览器标签改为WMS 2.去掉仓库拣货看板菜单,在数据库sysmenu 设置为hidemenu 3.修改appsettings.json

--- Front/PC/src/config/index.ts
+++ Front/PC/src/config/index.ts
@@ -4,7 +4,7 @@ import localSysZh from '@/locale/modules/zh-cn/system';
 const showLogo: Boolean = true; // 是否显示Logo顶部模块
 const systemTitle = 'message.system.title' // 系统名称，用于显示在左上角模块，以及浏览器标题上使用,使用配置项
 
-const appName='CHU DWES(Changsha Digital Warehouse Execution System)';
+const appName='WMS';
 const deftClassifyGroup:string=""
 const disableClassifyGroupSelect=false;
 
